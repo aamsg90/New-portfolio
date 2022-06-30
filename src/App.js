d64ccb2810838e4e34dc917353a57a1f12186e19
Ahmed Mohammed <aamsg90@gmail.com>
2022-06-30 10:33:36 -0400

    clone to office computer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import home from './pages/home';
+import projects from './pages/projects'; 
+import experience from './pages/experience';   
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route></Route>
+          <Route path="/" element={<home />}/>
+          <Route path="/projects" element={<projects />}/>
+          <Route path="/experience" element={<experience />}/>
         </Routes>
       </Router>
     </div>
